44df9732220fd97e80b626d17c48be013276641d
Arturs Cvetkovs <Galjwnte@gmail.com>
2017-12-25 14:12:26 +0200

    Hide api key, set default language, add autority for actuator, add application prod profile

--- src/main/java/eu/galjente/zooplus/ApplicationConfig.java
+++ src/main/java/eu/galjente/zooplus/ApplicationConfig.java
@@ -4,6 +4,10 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class ApplicationConfig {
@@ -11,6 +15,13 @@ public class ApplicationConfig {
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		return builder.build();
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
 	}
 
 }
